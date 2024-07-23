31e2e95eb715bbe7b3dd12c99b85a864f91dd596
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-07-23 14:19:07 +0530

    Add post_save signal methods for creating and saving profiles

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,6 +1,6 @@
-from typing import Iterable
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 from shortuuid.django_fields import ShortUUIDField
 
@@ -26,7 +26,7 @@ class User(AbstractUser):
         super(User, self).save(*args, **kwargs)
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
     full_name = models.CharField(max_length=100, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
@@ -43,10 +43,20 @@ class Profile(models.Model):
         if self.full_name:
             return str(self.full_name)
         else:
-            return str(self.user.full_name) # username instead of full_name?
+            return str(self.user.full_name) # username instead of full_name maybe?
         
     def save(self, *args, **kwargs):
         if self.full_name == "" or self.full_name == None:
             self.full_name = self.user.full_name
 
         super(Profile, self).save(*args, **kwargs)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
