a2a60104678540b582d68705cc0836f48793a9c0
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-07-23 14:35:17 +0530

    Make changes to user and profile admin.py under userauths which would be visible in admin url

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from userauths.models import Profile, User
 
-admin.site.register(User)
-admin.site.register(Profile)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name','email','phone']
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name','gender','country']
+    # list_editable = ['gender','country'] # Don't have first one of display as editable or it will throw an error
+    search_fields = ['full_name', 'date']
+    list_filter = ['date']
+    # Check Django documentation for configuring admin section to know more such fields, filters etc.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
