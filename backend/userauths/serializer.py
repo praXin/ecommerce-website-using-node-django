e8a243357e6c4ddb2f02800fd4b44e9f2d0949ec
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-07-24 20:33:40 +0530

    Add MyTokenObtainPairSerializer and RegisterSerializer classes in serializer.py
    In MyTokenObtainPairSerializer class, we override get_token method to return token for a User. In RegisterSerializer class, we validate user's password and create user(with entered password and save the user)

--- backend/userauths/serializer.py
+++ backend/userauths/serializer.py
@@ -1,6 +1,50 @@
+from django.contrib.auth.password_validation import validate_password
+
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from userauths.models import User, Profile
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Adding custom cleans to token
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        try:
+            token['vendor_id'] = user.vendor.id # user.vendor will be added to models later to show users associated to vendors
+        except:
+            token['vendor_id'] = 0
+
+        return token
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['full_name','email','phone','password ','password2'] # but user class in models.py doesn't have password and password2 fields
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name = validated_data['full_name'],
+            email = validated_data['email'],
+            phone = validated_data['phone']
+        )
+
+        email_user, mobile = user.email.split("@") # not sure if we're using email_user and mobile here
+        user.set_password(validate_password['password'])
+        user.save()
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -19,3 +63,4 @@ class ProfileSerializer(serializers.ModelSerializer):
         response = super().to_representation(instance)
         response['user'] = UserSerializer(instance.user).data
         return response
+    
