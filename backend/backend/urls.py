d677060bdf2dd78194ab992e000046d9ea8e14cb
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-07-25 18:40:13 +0530

    Add documentation for APIs and update requirements.txt
    Install drf-yasg (Swagger i.e. Django Rest Framework - Yet Another Swagger Generator) and add drf_yasg to INSTALLED_APPS of backend/settings.py along with shortuuid as we have installed both libraries externally. Run pip freeze to update requirements.txt. Finally, add API documentation (in backend/urls.py) using drf_yasg and test the urls: api/v1/user/register and api/v1/user/token. Also run pip freeze to update the requirements.txt file.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,30 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# below libraries are for APIs official documentation
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view =  get_schema_view(
+    openapi.Info(
+        title="E-Commerce Backend APIs",
+        default_version="v1",
+        description="This is documentation for the backend API",
+        terms_of_service="http://abc.com/policies",
+        contact=openapi.Contact(email="pravinkishore1997@gmail.com"),
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, )
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+
+    # Documentation
+    path("", schema_view.with_ui('swagger',cache_timeout=0), name="schema-swagger-ui")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
