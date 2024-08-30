ffe314493fa9abc98c3d20c4ba60bf3030032d00
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-08-30 14:14:17 +0530

    Add new model classes and change admin in store
    Add Gallery, Size, Specifications and Color models in store and also change and experiment around with admin

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from store.models import Product, Category
+from store.models import Product, Category, Gallery, Specification, Size, Color
+
+class GalleryInLine(admin.TabularInline):
+    model = Gallery
+    extra = 0
+
+class SpecificationInLine(admin.TabularInline):
+    model = Specification
+    extra = 0
+
+class SizeInLine(admin.TabularInline):
+    model = Size
+    extra = 0
+
+class ColorInLine(admin.TabularInline):
+    model = Color
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category', 'shipping_amount', 'stock_qty', 'in_stock', 'vendor', 'featured']
+    list_editable = ['featured']
+    list_filter = ['date']
+    search_fields = ['title']
+    inlines = [GalleryInLine, SpecificationInLine, SizeInLine, ColorInLine]
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
