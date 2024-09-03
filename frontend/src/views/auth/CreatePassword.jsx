f901d8b107502420786f44e63997e05b0033a31e
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-09-03 14:16:18 +0530

    Make changes and make UI better
    Make changes to UI files (frontend/src/views/auth) and improve code in many files such as Register, Login, ForgotPassword, CreatePassword. I still have to make changes to Dashboard to make it look better which I will do soon.

--- frontend/src/views/auth/CreatePassword.jsx
+++ frontend/src/views/auth/CreatePassword.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import { useSearchParams, useNavigate } from 'react-router-dom'
+import { useSearchParams, useNavigate, Link } from 'react-router-dom'
 import apiInstance from '../../utils/axios'
 
 function CreatePassword() {
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     const navigate = useNavigate()
     const [searchParam] = useSearchParams()
@@ -13,11 +14,14 @@ function CreatePassword() {
     const uidb64 = searchParam.get("uidb64")
     
     const handlePasswordSubmit = async (e) => {
+        setIsLoading(true)
         e.preventDefault()
 
         if(password !== confirmPassword){
             alert("Passwords don't match")
+            setIsLoading(false)
         } else {
+            setIsLoading(true)
             const formdata = new FormData()
             formdata.append('password',password)
             formdata.append('otp',otp)
@@ -28,38 +32,94 @@ function CreatePassword() {
                     console.log(res.data);
                     alert("Password changed successfully")
                     navigate("/login")
+                    setIsLoading(false)
                 })
             } catch (error) {
-                alert("An error occurred while trying to change the password")                
+                alert("An error occurred while trying to change the password") 
+                setIsLoading(false)
             }
         }
     }
     
     return (
-        <div>
-            <h1>Create new password</h1>
-            <form onSubmit={handlePasswordSubmit}>
-                <input 
-                    type="password" 
-                    name="" 
-                    id="" 
-                    placeholder="Enter new password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <br />
-                <br />
-                <input 
-                    type="password" 
-                    name="" 
-                    id="" 
-                    placeholder="Confirm new password"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-                <br />
-                <br />
-                <button type='submit'>Save new password</button>
-            </form>
-        </div>
+        <section>
+            <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+                <div className="container">
+                    {/* Section: Login form */}
+                    <section className="">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-xl-5 col-md-8">
+                                <div className="card rounded-5">
+                                    <div className="card-body p-4">
+                                        <h3 className="text-center">Forgot Password</h3>
+                                        <br />
+
+                                        <div className="tab-content">
+                                            <div
+                                                className="tab-pane fade show active"
+                                                id="pills-login"
+                                                role="tabpanel"
+                                                aria-labelledby="tab-login"
+                                            >
+                                                <form onSubmit={handlePasswordSubmit}>
+                                                    {/* Email input */}
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="loginName">
+                                                            Password
+                                                        </label>
+                                                        <input
+                                                            type="password"
+                                                            name="password"
+                                                            value={password}
+                                                            className="form-control"
+                                                            onChange={(e) => setPassword(e.target.value)}
+                                                        />
+                                                    </div>
+
+                                                    {/* Email input */}
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="loginName">
+                                                            Confirm Password
+                                                        </label>
+                                                        <input
+                                                            type="password"
+                                                            name="password"
+                                                            value={confirmPassword}
+                                                            className="form-control"
+                                                            onChange={(e) => setConfirmPassword(e.target.value)}
+                                                        />
+                                                    </div>
+
+                                                    {isLoading === true
+                                                        ? <button disabled className='btn btn-primary btn-rounded w-100 mb-4'>
+                                                            Processing...
+                                                        </button>
+
+                                                        : <button type='submit' className='btn btn-primary btn-rounded w-100 mb-4'>
+                                                            Save Password <i className='fas fa-check-circle' />
+                                                        </button>
+
+                                                    }
+
+                                                    {/* Sign-in button */}
+                                                    <div className="text-center">
+                                                        <p>
+                                                            Want to sign in? <Link to="/login">Login</Link>
+                                                        </p>
+                                                        {/* <button onClick={handleEmailSubmit} className='btn btn-primary w-100'>Reset Password</button> */}
+                                                    </div>
+
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </section>
     )
 }
 
--- frontend/src/views/auth/ForgotPassword.jsx
+++ frontend/src/views/auth/ForgotPassword.jsx
@@ -1,39 +1,95 @@
 import { useState } from 'react'
 import apiInstance from '../../utils/axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 function ForgotPassword() {
     const [email, setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     const navigate = useNavigate()
 
     const handleSubmit = async () => {
+        setIsLoading(true)
         try {
                 await apiInstance.get(`user/password-reset/${email}/`).then((res) => { // to pass variables in url, use backtick (tilde button on keyboard)
                     alert("An email has been sent to you.")
+                    setIsLoading(false)
                     // navigate("/create-new-password")
                     // console.log(res.data); // logging response data
             })
         } catch (error) {
             alert("Email does not exist.")
+            setIsLoading(false)
             // console.log(error);
         }
     }
 
     return (
-    <div>
-        <h1>Forgot Password</h1>
-        <input 
-            onChange={(e) => setEmail(e.target.value)} 
-            type="email" 
-            placeholder='Enter Email' 
-            name='' 
-            id='' 
-        />
-        <br />
-        <br />
-        <button onClick={handleSubmit}>Reset Password</button>
-    </div>
+        <section>
+            <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+                <div className="container">
+                    {/* Section: Login form */}
+                    <section className="">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-xl-5 col-md-8">
+                                <div className="card rounded-5">
+                                    <div className="card-body p-4">
+                                        <h3 className="text-center">Forgot Password</h3>
+                                        <br />
+
+                                        <div className="tab-content">
+                                            <div
+                                                className="tab-pane fade show active"
+                                                id="pills-login"
+                                                role="tabpanel"
+                                                aria-labelledby="tab-login"
+                                            >
+                                                <div>
+                                                    {/* Email input */}
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="loginName">
+                                                            Email Address
+                                                        </label>
+                                                        <input
+                                                            type="email"
+                                                            id="loginName"
+                                                            name="email"
+                                                            value={email}
+                                                            className="form-control"
+                                                            onChange={(e) => setEmail(e.target.value)}
+                                                        />
+                                                    </div>
+
+                                                    {isLoading === true
+                                                        ? <button disabled className='btn btn-primary btn-rounded w-100 mb-4'>
+                                                            Processing...
+                                                        </button>
+
+                                                        : <button onClick={handleSubmit} type='button' className='btn btn-primary btn-rounded w-100 mb-4'>
+                                                            Send Email <i className='fas fa-paper-plane' />
+                                                        </button>
+
+                                                    }
+
+                                                    {/* Sign-in button */}
+                                                    <div className="text-center">
+                                                        <p>
+                                                            Want to sign in? <Link to="/login">Login</Link>
+                                                        </p>
+                                                        {/* <button onClick={handleEmailSubmit} className='btn btn-primary w-100'>Reset Password</button> */}
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </section>
     )
 }
 
