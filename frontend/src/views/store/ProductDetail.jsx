72d97bcdf8733891ed83ce5133b17f984069cba9
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-09-05 23:00:05 +0530

    Add new file in plugin folder to generate a fixed length random ID (cart_id)
    Create and write code in src/views/plugin/CardID.jsx to return a random string in case there is no existing user maybe because user isn't logged in. Import CardID and call the function in ProductDetail.jsx and log the data.

--- frontend/src/views/store/ProductDetail.jsx
+++ frontend/src/views/store/ProductDetail.jsx
@@ -4,6 +4,9 @@ import { useParams } from 'react-router-dom'
 import apiInstance from '../../utils/axios'
 import GetCurrentAddress from '../plugin/UserCountry'
 import UserData from '../plugin/UserData'
+import CardID from '../plugin/CardID'
+
+
 
 function ProductDetail() {
     const [product, setProduct] = useState({})
@@ -19,6 +22,7 @@ function ProductDetail() {
     const param = useParams()
     const currentAddress = GetCurrentAddress()
     const userData = UserData()
+    const cart_id = CardID()
 
     
     
@@ -49,6 +53,7 @@ function ProductDetail() {
     const handleAddToCart = () => {
         console.log("User ID: ", userData?.user_id);
         console.log("Product ID:", product.id);
+        console.log("Cart ID:", cart_id);
         console.log("Price:", product.price); 
         console.log("Shipping Amount:", product.shipping_amount); 
         console.log("Qty:", qtyValue);
