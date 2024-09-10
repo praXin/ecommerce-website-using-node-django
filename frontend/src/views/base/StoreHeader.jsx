98c87648fd54b2e15e0ac8286c35318506e9a816
Koyalkar Pravin Kishore <pravinkishore1997@gmail.com>
2024-09-10 13:38:33 +0530

    Conditional rendering of store header links

--- frontend/src/views/base/StoreHeader.jsx
+++ frontend/src/views/base/StoreHeader.jsx
@@ -32,7 +32,7 @@ function StoreHeader() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">Pravins Test</Link>
+                    <Link className="navbar-brand" to="/">Pravin</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
